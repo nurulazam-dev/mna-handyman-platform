151eef19fb501d1e15bd3f47fabb0e8f172a29f2
mnaofficialbd <mna.official92@gmail.com>
2024-11-26 19:26:56 +0600

    Footer create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Router } from "react-router-dom";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <NavigationBar />
+        <Footer />
       </Router>
     </>
   );
