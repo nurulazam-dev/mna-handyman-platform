0fbc3d4a3e8583675ee4339b998abbc0e6b31121
mnaofficialbd <mna.official92@gmail.com>
2024-11-26 19:38:36 +0600

    Testimonials component create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Features from "../components/Features";
 import Hero from "../components/Hero";
+import PopularServices from "../components/PopularServices";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <PopularServices />
+      <Testimonials />
     </>
   );
 };
