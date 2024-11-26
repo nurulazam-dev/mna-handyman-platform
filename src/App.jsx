1efa9f1526e28d476049071ac9c35a39e231e04c
mnaofficialbd <mna.official92@gmail.com>
2024-11-26 19:32:06 +0600

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./components/Services";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import ServiceDetails from "./components/ServiceDetails";
+
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <NavigationBar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services/:id" element={<ServiceDetails />} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </>
