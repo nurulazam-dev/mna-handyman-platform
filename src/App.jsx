cd42927a036407581a70b6ff070b111c97a6e3c8
mnaofficialbd <mna.official92@gmail.com>
2024-11-26 19:25:26 +0600

    NavigationBar primary setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Router } from "react-router-dom";
 import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <NavigationBar />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,33 @@
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const NavigationBar = () => {
-  return <div>Navigation Bar</div>;
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          HandyMan Services
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
