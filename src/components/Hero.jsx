e0fa6e575c928b0dba039c4e1163267e3e89d81d
mnaofficialbd <mna.official92@gmail.com>
2024-12-02 23:40:14 +0600

    hero section update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 const Hero = () => {
   return (
     <section className="text-center bg-primary text-white p-5 mt-2 mb-4">
-      <h1 className=" mt-2">Welcome to HandyMan Services</h1>
+      <h1 className=" mt-2">Welcome to MNA HandyMan Services</h1>
       <p>Your one-stop platform for all repair and handyman needs.</p>
       <Link to="/services">
         <Button variant="light" className="m-2">
